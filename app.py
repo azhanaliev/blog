7fe00662790a466a779cf10d4c92272b1dd24bff
azhanaliev <70191382+azhanaliev@users.noreply.github.com>
2020-10-19 16:56:34 +0600

    'posts'

--- app.py
+++ app.py
@@ -30,6 +30,17 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template('posts.html', articles=articles)
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
+
+
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
     if request.method == "POST":
@@ -41,10 +52,9 @@ def create_article():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/')
+            return redirect('/posts')
         except:
             return "При добавлении статьи произошла ошибка"
-
     else:
         return render_template('create-article.html')
 
